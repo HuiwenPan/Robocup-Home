55027b6345d390a7a4db3b901c50c687845c72bb
Huiwen Pan <ga27keq@mytum.de>
2019-06-02 18:15:16 +0200

    initial workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/phw/GSW_champ/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/phw/bin:/home/phw/.local/bin:/home/phw/bin:/home/phw/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/home/phw/lib/pkgconfig"
+export PWD="/home/phw/GSW_champ/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/phw/GSW_champ/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/phw/GSW_champ/src:$ROS_PACKAGE_PATH"
